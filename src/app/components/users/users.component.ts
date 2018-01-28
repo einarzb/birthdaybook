ad1f041c3f5030c15ae646cb87ce23f01dac05a5
einarzb <einarzb@gmail.com>
2018-01-28 18:53:58 +0200

    fetching users from fib works

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+// import service and model
+import { UserService } from './../../services/user.service';
+import { User } from '../../models/User';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  users: User[];
 
-  constructor() { }
+  constructor( private userService: UserService) {}
 
+  //getUsers return observable that we need to subscribe to.
   ngOnInit() {
+    this.userService.getUsers().subscribe(users => 
+      this.users = users);
   }
 
 }
