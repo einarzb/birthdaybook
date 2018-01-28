23344ca5ee0a880b3a898a0175a4cda82d36ff99
einarzb <einarzb@gmail.com>
2018-01-28 18:04:06 +0200

    set angularfire2 and firebase setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyALWZyfq4PhJ29emcarP4_rCtRCghbIoIM",
+    authDomain: "birthday-ab963.firebaseapp.com",
+    databaseURL: "https://birthday-ab963.firebaseio.com",
+    projectId: "birthday-ab963",
+    storageBucket: "birthday-ab963.appspot.com",
+    messagingSenderId: "413308019989"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyALWZyfq4PhJ29emcarP4_rCtRCghbIoIM",
+    authDomain: "birthday-ab963.firebaseapp.com",
+    databaseURL: "https://birthday-ab963.firebaseio.com",
+    projectId: "birthday-ab963",
+    storageBucket: "birthday-ab963.appspot.com",
+    messagingSenderId: "413308019989"
+  }
 };
